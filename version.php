4894638fb92c19dd1c4f1a697d7cfa9d0382ae0e
lushonline <56685070+lushonline@users.noreply.github.com>
2022-08-08 11:47:20 +0100

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2022080103; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022080800; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700; // Requires this Moodle version v3.11 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'tool_percipioexternalcontentsync'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = ['mod_externalcontent' => ANY_VERSION];
