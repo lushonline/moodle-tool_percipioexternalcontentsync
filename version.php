4c3993fe62d75dcb95583f523f7cef98b959d38e
lushonline <56685070+lushonline@users.noreply.github.com>
2022-09-05 16:48:11 +0100

    Update version & dependency on mod_externalcontent

--- version.php
+++ version.php
@@ -25,10 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2022081100; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022090500; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700; // Requires this Moodle version v3.11 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'tool_percipioexternalcontentsync'; // Full name of the plugin (used for diagnostics).
-$plugin->dependencies = ['mod_externalcontent' => ANY_VERSION];
+$plugin->dependencies = ['mod_externalcontent' => 2022090500];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.1';
-
+$plugin->release = '2.0';
